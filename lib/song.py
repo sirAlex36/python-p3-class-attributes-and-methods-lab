2bda202f86629c7af2fdcc1dd3978b0b81abd887
SirAlex36 <alexngenoo@gmail.com>
2025-11-19 09:26:57 +0300

    initial commit

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,26 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists =[]
+    genre_count ={}
+    artist_count ={}
+    def __init__(self, name, artist,genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+        Song.genres.append(genre)
+        Song.artists.append(artist)
+
+        if genre in Song.genre_count:
+            Song.genre_count[genre] +=1
+        else:
+            Song.genre_count[genre] = 1
+
+        if artist in Song.artist_count:
+            Song.artist_count[artist] +=1
+        else:
+            Song.artist_count[artist] =1
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
